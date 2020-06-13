0cba4559a4429b54d4b76e97b064556d87f40eb0
Mukesh Rajput <msr5464@gmail.com>
2020-06-13 11:28:58 +0530

    update db backup again
    Signed-off-by: Mukesh Rajput <msr5464@gmail.com>

--- server/mysql-dump.sql
+++ server/mysql-dump.sql
@@ -4,6 +4,10 @@
 -- ------------------------------------------------------
 -- Server version	8.0.19
 
+DROP DATABASE IF EXISTS `thanos`;
+CREATE DATABASE `thanos`;
+USE `thanos`;
+
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
